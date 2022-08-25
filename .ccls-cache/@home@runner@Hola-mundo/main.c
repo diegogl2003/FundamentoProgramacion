78563e91ae65ef8f75a66f53b542ae0deeb5ca2d
Diego Ginera <>
2022-08-25 17:43:11 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@Hola-mundo/main.c
@@ -0,0 +1,14 @@
+/*Hola mundo
+//Diego Ginera
+//25-ago-2022
+//version 1 */
+
+//Bibliotecas
+#include <stdio.h>
+
+//Función principal 
+int main(void) {
+  //Funcion de impresion en pantalla
+  printf("hola mundo\n");
+  return 0;
+}
